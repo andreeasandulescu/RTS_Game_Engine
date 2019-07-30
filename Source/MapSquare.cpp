53a0b5f2610edd5f131f68f080c3b1d558477d94
Andrei-Alexandru TĂCHICI <andrei.tachici@stud.acs.upb.ro>
2019-07-30 13:18:51 +0300

    Added game logic classes.

---
+++ Source/MapSquare.cpp
@@ -0,0 +1,23 @@
+#include <MapSquare.h>
+
+/*
+ ^
+ x
+
+ V1(1,0,0) V0(1,0,1)
+  +--------+
+  |        |
+  |        |
+  |        |
+  |        |
+  +--------+
+V2(0,0,0) V3(0,0,1) ->z
+
+*/
+
+MapSquare::MapSquare() {
+	this->v0 = glm::vec3();
+	this->v0 = glm::vec3();
+	this->v0 = glm::vec3();
+	this->v0 = glm::vec3();
+}
